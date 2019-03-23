c8670dfa541a128dfa317f02dce93c93b023c27b
ianbunn <ianrichardbunn@gmail.com>
2019-03-23 10:53:29 -0500

    adding  to line 25 and line 58 in CarFrom/index.js

--- client/src/views/elements/CarForm/index.js
+++ client/src/views/elements/CarForm/index.js
@@ -20,67 +20,67 @@ export default class CarForm extends Component {
 // VIN
 
 <Form className="form-container">
-<Col md={{ size: 6, offset: 3 }}>
-<Row> <Col className="spacing">Please fill out the following information to update your vehicle</Col></Row>
-<Row form>
-  <Col md={3}>
- 
-    <FormGroup>
-      <Label for="Year">Year</Label>
-      <Input type="year" name="year" id="year" placeholder="" />
-    </FormGroup>
-  </Col>
-  <Col md={3}>
-    <FormGroup>
-      <Label for="Make">Make</Label>
-      <Input type="make" name="make" id="make" placeholder="" />
-    </FormGroup>
-  </Col>
-  <Col md={3}>
-    <FormGroup>
-      <Label for="Model">Model</Label>
-      <Input type="model" name="model" id="model" placeholder="" />
-    </FormGroup>
-  </Col>
-  <Col md={3}>
-    <FormGroup>
-      <Label for="Mileage">Mileage</Label>
-      <Input type="mileage" name="mileage" id="mileage" placeholder="" />
-    </FormGroup>
-  </Col>
-</Row>
-<Row>
- 
-  <Col className="spacing">OR</Col>
+  <Col md={{ size: 6, offset: 3 }}>
+  <Row> <Col className="spacing">Please fill out the following information to update your vehicle</Col></Row>
+  <Row form="true">
+    <Col md={3}>
   
-</Row>
+      <FormGroup>
+        <Label for="Year">Year</Label>
+        <Input type="year" name="year" id="year" placeholder="" />
+      </FormGroup>
+    </Col>
+    <Col md={3}>
+      <FormGroup>
+        <Label for="Make">Make</Label>
+        <Input type="make" name="make" id="make" placeholder="" />
+      </FormGroup>
+    </Col>
+    <Col md={3}>
+      <FormGroup>
+        <Label for="Model">Model</Label>
+        <Input type="model" name="model" id="model" placeholder="" />
+      </FormGroup>
+    </Col>
+    <Col md={3}>
+      <FormGroup>
+        <Label for="Mileage">Mileage</Label>
+        <Input type="mileage" name="mileage" id="mileage" placeholder="" />
+      </FormGroup>
+    </Col>
+  </Row>
+  <Row>
+  
+    <Col className="spacing">OR</Col>
+    
+  </Row>
 
-<Row form>
-  <Col md={3}>
+  <Row form="true">
+    <Col md={3}>
 
+    </Col>
+    <Col md={3}>
+      <FormGroup>
+        <Label for="vin">VIN Number</Label>
+        <Input type="vin" name="vin" id="vin" placeholder="" />
+      </FormGroup>
+    </Col>
+    <Col md={3}>
+      <FormGroup>
+      <Label for="Mileage">Mileage</Label>
+        <Input type="mileage" name="mileage" id="mileage" placeholder="" />
+      </FormGroup>
+    </Col>
+    <Col md={3}>
+    
+    </Col>
+  </Row>
+  <Row>
+    <Col className="spacing">
+  <Button>Update Vehicle</Button>
   </Col>
-  <Col md={3}>
-    <FormGroup>
-      <Label for="vin">VIN Number</Label>
-      <Input type="vin" name="vin" id="vin" placeholder="" />
-    </FormGroup>
+  </Row>
   </Col>
-  <Col md={3}>
-    <FormGroup>
-    <Label for="Mileage">Mileage</Label>
-      <Input type="mileage" name="mileage" id="mileage" placeholder="" />
-    </FormGroup>
-  </Col>
-  <Col md={3}>
-   
-  </Col>
-</Row>
-<Row>
-  <Col className="spacing">
-<Button>Update Vehicle</Button>
-</Col>
-</Row>
-</Col>
 </Form>
 
       
