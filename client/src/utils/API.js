05da472697c4adb3bcccb0cf6016a8d6d8d615f8
Speedway <harpalassi@gmail.com>
2019-03-23 13:18:20 -0500

    starting api calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+const APIKEY = `&key=${process.env.REACT_APP_CARMD_AUTH_KEY}`;
+const PARTNER_TOKEN = process.env.REACT_APP_CARMD_PARTNER_TOKEN; 
+
+// export default {
+//   // gets books from google books api
+//   search: function(query) {
+//     return axios.get(BASEURL + query + APIKEY + PARTNER_TOKEN);
+//   },
+
 export default {
   // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  getAvailableFields: function(query)
   },
   // Gets the book with the given id
   getBook: function(id) {
