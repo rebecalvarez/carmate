4a182a84f5bccda9c4ea2228ad03a9c291ec7977
Speedway <harpalassi@gmail.com>
2019-03-27 12:42:12 -0500

    added getavailablefields

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASEURL = 'https://www.googleapis.com/books/v1/volumes?q=';
-const APIKEY = `&key=${process.env.REACT_APP_CARMD_AUTH_KEY}`;
+const BASEURL = 'http://api.carmd.com/v3.0/fields?';
+const AUTH_KEY = `Basic ${process.env.REACT_APP_CARMD_AUTH_KEY}`;
 const PARTNER_TOKEN = process.env.REACT_APP_CARMD_PARTNER_TOKEN;
 
 // export default {
@@ -11,19 +11,31 @@ const PARTNER_TOKEN = process.env.REACT_APP_CARMD_PARTNER_TOKEN;
 //   },
 
 export default {
-  // Gets all books
-  // getAvailableFields: function(query)
+  //get available fields
+  getAvailableFields: function(vin, mileage) {
+    console.log('authorization:' + AUTH_KEY, 'partner-token:' + PARTNER_TOKEN);
+    vin = `vin=${vin}&`;
+    mileage = `mileage=${mileage}`;
+    return axios.get(BASEURL + 'vin=1GNALDEK9FZ108495&' + mileage, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: AUTH_KEY,
+        'Partner-Token': PARTNER_TOKEN,
+      },
+    });
+  },
+
+  // // Gets the book with the given id
+  // getBook: function(id) {
+  //   return axios.get('/api/books/' + id);
   // },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get('/api/books/' + id);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete('/api/books/' + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post('/api/books', bookData);
-  },
+  // // Deletes the book with the given id
+  // deleteBook: function(id) {
+  //   return axios.delete('/api/books/' + id);
+  // },
+  // // Saves a book to the database
+  // saveBook: function(bookData) {
+  //   return axios.post('/api/books', bookData);
+  // },
 };
--- client/src/views/elements/CarForm/index.js
+++ client/src/views/elements/CarForm/index.js
@@ -1,89 +1,90 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, Col, Row } from 'reactstrap';
-import "./style.css";
-
+import './style.css';
+import API from '../../../utils/API';
 
 export default class CarForm extends Component {
   constructor() {
     super();
-    this.state = { }
+    this.state = {};
   }
+
+  getFields = (vin = '1GNALDEK9FZ108495', mileage = '55000') => {
+    API.getAvailableFields(vin, mileage)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
+      // Year
+      // Make
+      // Model
+      // Mileage
+      // OR
+      // VIN
 
-// Year
-// Make
-// Model
-// Mileage
-// OR
-// VIN
+      <Form className="form-container">
+        <Col md={{ size: 6, offset: 3 }}>
+          <Row>
+            {' '}
+            <Col className="spacing">Please fill out the following information to update your vehicle</Col>
+          </Row>
+          <Row form="true">
+            <Col md={3}>
+              <FormGroup>
+                <Label for="Year">Year</Label>
+                <Input type="year" name="year" id="year" placeholder="" />
+              </FormGroup>
+            </Col>
+            <Col md={3}>
+              <FormGroup>
+                <Label for="Make">Make</Label>
+                <Input type="make" name="make" id="make" placeholder="" />
+              </FormGroup>
+            </Col>
+            <Col md={3}>
+              <FormGroup>
+                <Label for="Model">Model</Label>
+                <Input type="model" name="model" id="model" placeholder="" />
+              </FormGroup>
+            </Col>
+            <Col md={3}>
+              <FormGroup>
+                <Label for="Mileage">Mileage</Label>
+                <Input type="mileage" name="mileage" id="mileage" placeholder="" />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="spacing">OR</Col>
+          </Row>
 
-<Form className="form-container">
-  <Col md={{ size: 6, offset: 3 }}>
-  <Row> <Col className="spacing">Please fill out the following information to update your vehicle</Col></Row>
-  <Row form="true">
-    <Col md={3}>
-  
-      <FormGroup>
-        <Label for="Year">Year</Label>
-        <Input type="year" name="year" id="year" placeholder="" />
-      </FormGroup>
-    </Col>
-    <Col md={3}>
-      <FormGroup>
-        <Label for="Make">Make</Label>
-        <Input type="make" name="make" id="make" placeholder="" />
-      </FormGroup>
-    </Col>
-    <Col md={3}>
-      <FormGroup>
-        <Label for="Model">Model</Label>
-        <Input type="model" name="model" id="model" placeholder="" />
-      </FormGroup>
-    </Col>
-    <Col md={3}>
-      <FormGroup>
-        <Label for="Mileage">Mileage</Label>
-        <Input type="mileage" name="mileage" id="mileage" placeholder="" />
-      </FormGroup>
-    </Col>
-  </Row>
-  <Row>
-  
-    <Col className="spacing">OR</Col>
-    
-  </Row>
-
-  <Row form="true">
-    <Col md={3}>
-
-    </Col>
-    <Col md={3}>
-      <FormGroup>
-        <Label for="vin">VIN #</Label>
-        <Input type="vin" name="vin" id="vin" placeholder="" />
-      </FormGroup>
-    </Col>
-    <Col md={3}>
-      <FormGroup>
-      <Label for="Mileage">Mileage</Label>
-        <Input type="mileage" name="mileage" id="mileage" placeholder="" />
-      </FormGroup>
-    </Col>
-    <Col md={3}>
-    
-    </Col>
-  </Row>
-  <Row>
-    <Col className="spacing">
-  <Button className="button-style">Update Vehicle</Button>
-  </Col>
-  </Row>
-  </Col>
-</Form>
-
-      
-    )
+          <Row form="true">
+            <Col md={3} />
+            <Col md={3}>
+              <FormGroup>
+                <Label for="vin">VIN #</Label>
+                <Input type="vin" name="vin" id="vin" placeholder="" />
+              </FormGroup>
+            </Col>
+            <Col md={3}>
+              <FormGroup>
+                <Label for="Mileage">Mileage</Label>
+                <Input type="mileage" name="mileage" id="mileage" placeholder="" />
+              </FormGroup>
+            </Col>
+            <Col md={3} />
+          </Row>
+          <Row>
+            <Col className="spacing">
+              <Button className="button-style" onClick={this.getFields}>
+                Update Vehicle
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Form>
+    );
   }
 }
