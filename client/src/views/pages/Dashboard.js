14192da18ab0a133d36d32c5e810ae1b468b3613
Speedway <harpalassi@gmail.com>
2019-03-30 12:49:22 -0500

    consequence

--- client/src/views/pages/Dashboard.js
+++ client/src/views/pages/Dashboard.js
@@ -85,10 +85,10 @@ class Dashboard extends Component {
 
   handleFormSubmit = event => {
     event.preventDefault();
-    this.getMaintenance(this.state.year, this.state.make, this.state.model, this.state.mileage, this.state.vin);
+    // this.getMaintenance(this.state.year, this.state.make, this.state.model, this.state.mileage, this.state.vin);
     this.getRecalls(this.state.year, this.state.make, this.state.model, this.state.vin);
-    this.getUpcoming(this.state.year, this.state.make, this.state.model, this.state.mileage, this.state.vin);
-    this.getWarranty(this.state.year, this.state.make, this.state.model, this.state.vin);
+    // this.getUpcoming(this.state.year, this.state.make, this.state.model, this.state.mileage, this.state.vin);
+    // this.getWarranty(this.state.year, this.state.make, this.state.model, this.state.vin);
   };
 
   render() {
