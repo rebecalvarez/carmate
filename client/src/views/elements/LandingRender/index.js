697262da04cf58f25b0ee76841f70bed7d3dfc4a
NicolaosJM <nicmay@gmail.com>
2019-04-04 21:12:09 -0500

    added login component to the bottom of landing, after query

--- client/src/views/elements/LandingRender/index.js
+++ client/src/views/elements/LandingRender/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import Icon from './images/iconcheck.png';
 import GoogleLogin from 'react-google-login';
+import Login from '../../../vibe/components/Login/Login'
 
 function LandingRender(props) {
   return (
@@ -60,7 +61,8 @@ function LandingRender(props) {
         <div>
           <h4 className="rendersubtitle">
             Create an account to see more details...
-            <GoogleLogin />
+            <Login />
+            
           </h4>
         </div>
       ) : null}
