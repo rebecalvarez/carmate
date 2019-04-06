b7fca3065234fa3e3a407c681346aa40b2cb7075
ianbunn <ianrichardbunn@gmail.com>
2019-04-05 20:31:59 -0500

    added react-addons-update so npm install, added getUserServices to the client API, added componentDidMount logic, added userservices logic route to the backend

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -76,24 +76,9 @@ export default {
 
   saveUser: (userData) => {
     return axios.post('api/user', userData);
+  },
+
+  getUserServices: (userEmail) => {
+    return axios.get('api/userservices', userEmail);
   }
-
-  // getUser: () => {
-  //   return axios.get('api/user', userData);
-  // }
-
-
-
-  // // Gets the book with the given id
-  // getBook: function(id) {
-  //   return axios.get('/api/books/' + id);
-  // },
-  // // Deletes the book with the given id
-  // deleteBook: function(id) {
-  //   return axios.delete('/api/books/' + id);
-  // },
-  // // Saves a book to the database
-  // saveBook: function(bookData) {
-  //   return axios.post('/api/books', bookData);
-  // },
 };
